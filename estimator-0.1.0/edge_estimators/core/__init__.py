2f1d7e8fbeac00ccc73ead4037029c95842a07bc
Ankitkarki <karkiankit101@gmail.com>
2026-01-15 23:54:00 -0600

    feat: complete renaming and code cleanup

--- estimator-0.1.0/edge_estimators/core/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Core estimator orchestration and state management."""
-
-from edge_estimators.core.estimator import EKF, KF, UKF
-from edge_estimators.core.state import State
-
-__all__ = ["KF", "EKF", "UKF", "State"]
